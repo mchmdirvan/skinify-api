cb8626f6360433db9e2f3cbcc5f5f51787463022
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-04 20:25:54 +0700

    feat: create product model

---
+++ prisma/migrations/20250804132518_products/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "sku" TEXT,
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "stockQuantity" INTEGER NOT NULL DEFAULT 0,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "public"."Product"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_sku_key" ON "public"."Product"("sku");
