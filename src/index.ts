0caa823fc9080f8923e2640c760c47c15fe5185d
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-08 06:23:15 +0700

    chore: setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 import { prisma } from "./utils/prisma";
 
 const app = new Hono();
+
+app.use(cors());
 
 app.get("/", (c) => {
   return c.text("Skinify Backend API");
