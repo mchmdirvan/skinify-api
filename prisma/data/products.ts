e1f28700dde3d1a69ee329e51cadb69bff4e60a5
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-24 10:03:10 +0700

    refactor: add model slug

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -2,12 +2,19 @@ import { Product } from "../../src/generated/prisma";
 
 type SeedProduct = Pick<
   Product,
-  "name" | "sku" | "description" | "imageUrl" | "stockQuantity" | "price"
+  | "name"
+  | "modelSlug"
+  | "sku"
+  | "description"
+  | "imageUrl"
+  | "stockQuantity"
+  | "price"
 >;
 
 export const dataProducts: SeedProduct[] = [
   {
     name: "iPhone 16 Skins - Slate",
+    modelSlug: "iphone-16",
     sku: "SKINIFY-IP16-SLATE",
     description:
       "Let’s get one thing straight — your iPhone 16 is cool. But not your kind of cool. Wrap your iPhone 16 with Exacoat’s premium skin – no added bulk, just flawless defense. Engineered for an exact, edge‑to‑edge fit.",
