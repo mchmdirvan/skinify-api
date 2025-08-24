0296343242246c5e4b6f8dc098ff04f957c38848
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-24 13:56:33 +0700

    refactor: remove product description

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -2,22 +2,14 @@ import { Product } from "../../src/generated/prisma";
 
 type SeedProduct = Pick<
   Product,
-  | "name"
-  | "modelSlug"
-  | "sku"
-  | "description"
-  | "imageUrl"
-  | "stockQuantity"
-  | "price"
+  "name" | "modelSlug" | "sku" | "imageUrl" | "stockQuantity" | "price"
 >;
 
 export const dataProducts: SeedProduct[] = [
   {
     name: "iPhone 16 Skins - Slate",
-    modelSlug: "iphone-16",
     sku: "SKINIFY-IP16-SLATE",
-    description:
-      "Let’s get one thing straight — your iPhone 16 is cool. But not your kind of cool. Wrap your iPhone 16 with Exacoat’s premium skin – no added bulk, just flawless defense. Engineered for an exact, edge‑to‑edge fit.",
+    modelSlug: "iphone-16",
     imageUrl:
       "https://ucarecdn.com/f650359b-3692-4e47-9450-8caabf85999c/Screenshot_20250818_151029removebgpreview.png",
     stockQuantity: 112,
