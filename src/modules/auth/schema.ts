403aa09b947f787783f8fc7ad3093fe0e4e7969f
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-25 15:08:48 +0700

    feat: create auth schema

---
+++ src/modules/auth/schema.ts
@@ -0,0 +1,23 @@
+import { z } from "@hono/zod-openapi";
+
+export const AuthRegisterSchema = z.object({
+  email: z.string(),
+  username: z.string(),
+  fullName: z.string(),
+  password: z.string(),
+});
+
+export const AuthLoginSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
+export const AuthMeSchema = z.object({
+  id: z.string(),
+  email: z.string(),
+  username: z.string(),
+  fullName: z.string(),
+  password: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
