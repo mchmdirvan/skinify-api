39c1944d44fc015142de4984790ae927843b6b70
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-08-25 15:00:22 +0700

    fix: import zod from hono zod openapi

--- src/modules/brand/schema.ts
+++ src/modules/brand/schema.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import { z } from "@hono/zod-openapi";
 
 export const BrandSchema = z.object({
   id: z.string(),
--- src/modules/model/schema.ts
+++ src/modules/model/schema.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import { z } from "@hono/zod-openapi";
 
 export const ModelSchema = z.object({
   id: z.string(),
